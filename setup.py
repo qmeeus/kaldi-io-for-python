7e52523abeb8390d60966afa7541b216139d9ac5
Karel Vesely <karel.vesely@soapboxlabs.com>
2019-02-07 17:33:02 +0100

    renaming 'kaldi_io/__init__.py' -> 'kaldi_io/kaldi_io.py',

--- setup.py
+++ setup.py
@@ -1,6 +1,11 @@
 #!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+# Copyright 2019  Oplatai.com (author: Ondrej Platek)
+# Licensed under the Apache License, Version 2.0 (the "License")
+
 from setuptools import setup, find_packages
-setup(name='kaldi_IO',
+setup(name='kaldi_io',
         version='0.0.1',
         description='Glue code connecting Kaldi data and Python.',
         author='Karel Vesely',
