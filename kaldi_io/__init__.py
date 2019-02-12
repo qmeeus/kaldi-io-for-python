b02b09ce6d59573d334f7ad72900751986b4dae9
Karel Vesely <vesis84@gmail.com>
2019-02-12 13:58:18 +0100

    refactoring the codebase,
    - rewriting unit-tests to use python 'unittest' framework,
    -- creating 'tests' directory, the data-files are moved there 'tests/data/',
    - changing indent of 'kaldi_io.py' to 1tab=4space,
    - require forward compatibility of prints by import from '__future__',
    - fixing a bug in 'kaldi_io/__init__.py',

--- kaldi_io/__init__.py
+++ kaldi_io/__init__.py
@@ -4,10 +4,13 @@
 # Copyright 2019  Brno University of Technology (author: Karel Vesely)
 # Licensed under the Apache License, Version 2.0 (the "License")
 
-# just import the main module,
-from kaldi_io import *
+# we will be importing from '../'
+from __future__ import absolute_import
 
-# We might later subdivide the project into more modules, 
-# and the interface will stay the same, if all modules 
+# just import the main module,
+from kaldi_io.kaldi_io import *
+
+# We might later subdivide the project into more modules,
+# and the interface will stay the same, if all modules
 # are listed in this '__init__.py'.
 
